ec42c9facf839ac03b830ee946f3059f67719074
Lakshyadeep Gogoi <lakshyadeep19@gmail.com>
2023-03-28 23:40:37 +0530

    JS
    Js file has been started and last generate random password function done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const inputSlider = document.querySelector("[data-lenghtSlider]");
-const lengthDisplay = document.querySelector("[data-lengthNumber]"); 
+const lengthDisplay = document.querySelector("[data-lenghtNumber]"); 
 const passwordDisplay = document.querySelector("[data-passwordDisplay]")
 const copyBtn = document.querySelector("[data-copy]");
 const copyMsg = document.querySelector("[data-copyMsg]");
@@ -10,10 +10,47 @@ const symbolsCheck = document.querySelector("#symbols");
 const indicator = document.querySelector("[data-indicator]");
 const generateBtn = document.querySelector(".generateButton");
 const allCheckBox = document.querySelectorAll("input[type=checkbox]")
-
+const symbols = '~`!@#$%^&*()_-=+{}[]|\:;"<,>.?/';
 
 let password = "";
 let passwordLength = 10;
 let checkCount = 1;
+handleSlider();
 //Set strength color to grey
 
+
+//Set passwordLength
+function handleSlider(){
+    inputSlider.value= passwordLength;
+    lengthDisplay.innerText = passwordLength;
+    //anything Else
+
+}
+
+function setIndicator(color){
+    indicator.style.backgroundColor = color;
+    //Shadow
+}
+
+function getRndInteger(min,max){
+    return Math.floor(Math.random() = (max - min)) + min;
+}
+
+function generateRandomNumber(){
+    return getRndInteger(0,9);
+}
+
+function generateLowerCase(){
+    return string.fromCharCode( getRndInteger(97,123));
+
+}
+
+function generateUpperCase(){
+    return string.fromCharCode( getRndInteger(65,90));
+
+}
+function generateSymbol(){
+    const random = getRndInteger(0, symbols.length);
+    return symbols.charAt(random);
+
+}
