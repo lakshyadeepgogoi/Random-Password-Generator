6775d838360ab5d4c77d1ae719276c3ad873e09d
Lakshyadeep Gogoi <lakshyadeep19@gmail.com>
2023-03-30 00:01:20 +0530

    css code is in process

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ let passwordLength = 10;
 let checkCount = 0;
 handleSlider();
 //ste strength circle color to grey
+setIndicator('#ccc');
 
 
 //set passwordLength
@@ -27,11 +28,15 @@ function handleSlider() {
     inputSlider.value = passwordLength;
     lengthDisplay.innerText = passwordLength;
     //or kuch bhi karna chahiye ? - HW
+    const min = inputSlider.min;
+    const max = inputSlider.max;
+    inputSlider.style.backgroundSize = ( (passwordLength - min)*100/(max - min)) + "% 100%"
 }
 
 function setIndicator(color) {
     indicator.style.backgroundColor = color;
     //shadow - HW
+    indicator.style.boxShadow = `0px 0px 12px 1px ${color}`;
 }
 
 function getRndInteger(min, max) {
@@ -98,7 +103,9 @@ async function copyContent() {
 function shufflePassword(array) {
     //Fisher Yates Method
     for (let i = array.length - 1; i > 0; i--) {
+        //random j,find out using random function
         const j = Math.floor(Math.random() * (i + 1));
+        //swap number at i index and j index
         const temp = array[i];
         array[i] = array[j];
         array[j] = temp;
@@ -153,25 +160,6 @@ generateBtn.addEventListener('click', () => {
     console.log("Starting the Journey");
     //remove old password
     password = "";
-
-    //let's put the stuff mentioned by checkboxes
-
-    // if(uppercaseCheck.checked) {
-    //     password += generateUpperCase();
-    // }
-
-    // if(lowercaseCheck.checked) {
-    //     password += generateLowerCase();
-    // }
-
-    // if(numbersCheck.checked) {
-    //     password += generateRandomNumber();
-    // }
-
-    // if(symbolsCheck.checked) {
-    //     password += generateSymbol();
-    // }
-
     let funcArr = [];
 
     if(uppercaseCheck.checked)
